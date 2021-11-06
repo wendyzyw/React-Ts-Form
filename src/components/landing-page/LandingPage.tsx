726fe90f6258213c445c3cd401ace3c87cf15891
Wendy Zeng <wendy.zeng@iress.com>
2021-11-06 22:26:37 +1100

    feat: branding landing page

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -1,8 +1,14 @@
+import Button from '@mui/material/Button';
 
 const LandingPage = () => {
     return (
         <div id="main-content">
-            
+            <div className="row">
+                <h1>A better way </h1>
+                <h1>to enjoy every day. </h1>
+                <p>Be the first to know when we launch.</p>
+                <Button variant="contained" style={{textTransform: "none", padding: "10px 40px"}}>Request an invite</Button>
+            </div>
         </div>
     );
 }
