c9a8b91e0a881d99a8d0cd86fa4d75791acc037f
Wendy Zeng <wendy.zeng@iress.com>
2021-11-07 01:59:00 +1100

    fix: form styling

--- src/components/request-form-modal/RequestFormModal.tsx
+++ src/components/request-form-modal/RequestFormModal.tsx
@@ -18,48 +18,42 @@ export const RequestFormModal: FC<RequestFormModalProps> = ({
             <DialogTitle>Request an invite</DialogTitle>
             <p>Form</p>
             <DialogContent>
-                <div>
-                    <TextField
-                        autoFocus
-                        id="fullname"
-                        label="Full name"
-                        type="text"
-                        variant="standard"
-                        margin="dense"
-                        fullWidth
-                    />
-                </div>
-                <div>
-                    <TextField
-                        autoFocus
-                        id="email"
-                        label="Email"
-                        type="email"
-                        variant="standard"
-                        margin="dense"
-                        fullWidth
-                    />
-                </div>
-                <div>
-                    <TextField
-                        autoFocus
-                        id="confirmEmail"
-                        label="Confirm email"
-                        type="email"
-                        variant="standard"
-                        margin="dense"
-                        fullWidth
-                    />
-                </div>
+                <TextField
+                    autoFocus
+                    id="fullname"
+                    label="Full name"
+                    type="text"
+                    variant="outlined"
+                    margin="dense"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    id="email"
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    margin="dense"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    id="confirmEmail"
+                    label="Confirm email"
+                    type="email"
+                    variant="outlined"
+                    margin="dense"
+                    fullWidth
+                />
+                <DialogActions>
+                    <Button
+                        variant="contained"
+                        style={{ textTransform: "none", padding: "10px 40px", width: "100%" }}
+                    >
+                        Submit
+                    </Button>
+                </DialogActions>
             </DialogContent>
-            <DialogActions>
-                <Button
-                    variant="contained"
-                    style={{ textTransform: "none", padding: "10px 40px", width: "195px" }}
-                >
-                    Submit
-                </Button>
-            </DialogActions>
         </Dialog>
     )
 }
